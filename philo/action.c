ca22624fc87dee13e9cd36dad228d630ce23239e
xhseb <zzzu5280@gmail.com>
2021-11-14 23:40:18 +0900

    refactor

--- philo/action.c
+++ philo/action.c
@@ -6,7 +6,7 @@
 /*   By: sonkang <sonkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 22:21:00 by sonkang           #+#    #+#             */
-/*   Updated: 2021/11/09 22:21:03 by sonkang          ###   ########.fr       */
+/*   Updated: 2021/11/14 23:22:06 by sonkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	ph_die(t_ph *ph)
 {
 	if (present(ph) - ph->last_eat >= ph->in->die_t)
 	{
-		printf("time : %u, philo%d is died\n", present(ph), ph->id);
+		pthread_mutex_lock(&(ph->in->fork[ph->id - 1]));
+		printf("%u %d died\n", present(ph), ph->id);
+
 		ph->die = 1;
 		return (1);
 	}
@@ -25,13 +27,13 @@ int	ph_die(t_ph *ph)
 
 int	ph_think(t_ph *ph)
 {
-	printf("time : %u, philo%d is thinking\n", present(ph), ph->id);
+	printf("%u %d is thinking\n", present(ph), ph->id);
 	return (0);
 }
 
 int	ph_sleep(t_ph *ph)
 {
-	printf("time : %u, philo%d is sleeping\n", present(ph), ph->id);
+	printf("%u %d is sleeping\n", present(ph), ph->id);
 	ph->sl_st = present(ph);
 	doing(ph, ph->in->sleep_t, ph->sl_st);
 	if (ph_die(ph))
@@ -55,8 +57,8 @@ int	fork_mutex(t_ph *ph)
 		pthread_mutex_unlock(&(ph->in->fork[ph->id - 1]));
 		return (1);
 	}
-	printf("time : %u, philo%d has taken a fork\n", present(ph), ph->id);
-	printf("time : %u, philo%d is %d eating\n", present(ph), ph->id, ++ph->eat);
+	printf("%u %d has taken a fork\n", present(ph), ph->id);
+	printf("%u %d is eating\n", present(ph), ph->id, ++ph->eat);
 	ph->eat_st = present(ph);
 	doing(ph, ph->in->eat_t, ph->eat_st);
 	pthread_mutex_unlock(&(ph->in->fork[fork]));
